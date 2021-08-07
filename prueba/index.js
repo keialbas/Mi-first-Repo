ae4b8deaf2bc3aa49ed834f49bb04bc450172bca
Keyvin Alvarez <keialbas@gmail.com>
2021-08-07 12:38:20 -0600

    nuevo branch

--- prueba/index.js
+++ prueba/index.js
@@ -3,13 +3,20 @@ var numbers =[1,2,4,1,23,3,5,2,43,45,346,6,546,45,7,457,45,6,5,2,12,1,23,1,312,3
 numbers.sort()
 console.log(numbers.length)
 var count = 1;
-for(i =0; i<numbers.length-1; i++){
+for(let i =0; i<numbers.length-1; i++){
     if (numbers[i] == numbers[i+1]){
     count ++
     }
     else{
-        console.log(numbers[i]+"="+count)
+      //  console.log(numbers[i]+"="+count)
         count =12
+    }
+
+}
+
+for (let i =0 ; i< numbers.length; i++){
+    if(numbers[i] % 2 === 1){
+        console.log(Math.pow(numbers[i], 2))
     }
 
 }
